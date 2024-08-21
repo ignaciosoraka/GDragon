8fa1ac88a0da274cde52bfd5ac60dee676ec06cd
ignaciosoraka <ignaciosoraka@gmail.com>
2024-08-21 13:36:37 -0300

    final

--- java/nav.js
+++ java/nav.js
@@ -1,22 +1,33 @@
 const toggleMenu = () => {
-    const navigation = document.querySelector(".navigation");
-  
-    const burgerMenu = document.querySelector(".menu-icon");
-    const src = burgerMenu.getAttribute("src");
-  
-    const isBurger = src === "images/burger-menu.svg";
-    const iconName = isBurger ? "images/close.svg" : "images/burger-menu.svg";
-  
-    burgerMenu.setAttribute("src", iconName);
-  
-    if (!isBurger) {
+  const navigation = document.querySelector(".navigation");
+  const burgerMenu = document.querySelector(".menu-icon");
+  const src = burgerMenu.getAttribute("src");
+
+  const isBurger = src === "images/burger-menu.svg";
+  const iconName = isBurger ? "images/close.svg" : "images/burger-menu.svg";
+
+  burgerMenu.setAttribute("src", iconName);
+
+  if (!isBurger) {
       navigation.classList.add("navigation--mobile--fadeout");
       setTimeout(() => {
-        navigation.classList.toggle("navigation--mobile");
+          navigation.classList.toggle("navigation--mobile");
       }, 300);
-    } else {
+  } else {
       navigation.classList.remove("navigation--mobile--fadeout");
       navigation.classList.toggle("navigation--mobile");
-    }
-  };
-  
+  }
+};
+
+// Añadir evento para cerrar el menú al hacer clic en un enlace de navegación
+document.querySelectorAll('.navigation a').forEach(link => {
+  link.addEventListener('click', () => {
+      const navigation = document.querySelector(".navigation");
+      const burgerMenu = document.querySelector(".menu-icon");
+
+      // Solo cerrar el menú si está abierto
+      if (navigation.classList.contains("navigation--mobile")) {
+          toggleMenu();
+      }
+  });
+});
